1e20da67e18cc369d8fee12805ca1b746d12ffe2
hoangxi1202 <huynhdominhhoang6601@gmail.com>
2022-07-03 16:10:49 +0700

    login, main, member

--- SalesWinApp/frmMemberDetail.Designer.cs
+++ SalesWinApp/frmMemberDetail.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MyStoreWinApp
+﻿namespace SalesWinApp
 {
     partial class frmMemberDetail
     {
@@ -28,162 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbMemberID = new System.Windows.Forms.Label();
-            this.lbCompanyName = new System.Windows.Forms.Label();
+            this.lbMemberId = new System.Windows.Forms.Label();
             this.lbEmail = new System.Windows.Forms.Label();
-            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbCompanyName = new System.Windows.Forms.Label();
             this.lbCity = new System.Windows.Forms.Label();
             this.lbCountry = new System.Windows.Forms.Label();
-            this.txtMemberID = new System.Windows.Forms.TextBox();
-            this.txtCity = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtCountry = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtCompanyName = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
+            this.lbPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lbMemberID
+            // lbMemberId
             // 
-            this.lbMemberID.AutoSize = true;
-            this.lbMemberID.Location = new System.Drawing.Point(47, 32);
-            this.lbMemberID.Name = "lbMemberID";
-            this.lbMemberID.Size = new System.Drawing.Size(84, 20);
-            this.lbMemberID.TabIndex = 0;
-            this.lbMemberID.Text = "Member ID";
-            // 
-            // lbCompanyName
-            // 
-            this.lbCompanyName.AutoSize = true;
-            this.lbCompanyName.Location = new System.Drawing.Point(47, 69);
-            this.lbCompanyName.Name = "lbCompanyName";
-            this.lbCompanyName.Size = new System.Drawing.Size(116, 20);
-            this.lbCompanyName.TabIndex = 1;
-            this.lbCompanyName.Text = "Company Name";
+            this.lbMemberId.AutoSize = true;
+            this.lbMemberId.Location = new System.Drawing.Point(43, 35);
+            this.lbMemberId.Name = "lbMemberId";
+            this.lbMemberId.Size = new System.Drawing.Size(84, 20);
+            this.lbMemberId.TabIndex = 0;
+            this.lbMemberId.Text = "Member ID";
             // 
             // lbEmail
             // 
             this.lbEmail.AutoSize = true;
-            this.lbEmail.Location = new System.Drawing.Point(47, 107);
+            this.lbEmail.Location = new System.Drawing.Point(43, 70);
             this.lbEmail.Name = "lbEmail";
             this.lbEmail.Size = new System.Drawing.Size(46, 20);
-            this.lbEmail.TabIndex = 2;
+            this.lbEmail.TabIndex = 1;
             this.lbEmail.Text = "Email";
             // 
-            // lbPassword
+            // lbCompanyName
             // 
-            this.lbPassword.AutoSize = true;
-            this.lbPassword.Location = new System.Drawing.Point(47, 147);
-            this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(70, 20);
-            this.lbPassword.TabIndex = 3;
-            this.lbPassword.Text = "Password";
+            this.lbCompanyName.AutoSize = true;
+            this.lbCompanyName.Location = new System.Drawing.Point(43, 105);
+            this.lbCompanyName.Name = "lbCompanyName";
+            this.lbCompanyName.Size = new System.Drawing.Size(116, 20);
+            this.lbCompanyName.TabIndex = 2;
+            this.lbCompanyName.Text = "Company Name";
             // 
             // lbCity
             // 
             this.lbCity.AutoSize = true;
-            this.lbCity.Location = new System.Drawing.Point(47, 185);
+            this.lbCity.Location = new System.Drawing.Point(43, 140);
             this.lbCity.Name = "lbCity";
             this.lbCity.Size = new System.Drawing.Size(34, 20);
-            this.lbCity.TabIndex = 4;
+            this.lbCity.TabIndex = 3;
             this.lbCity.Text = "City";
             // 
             // lbCountry
             // 
             this.lbCountry.AutoSize = true;
-            this.lbCountry.Location = new System.Drawing.Point(47, 223);
+            this.lbCountry.Location = new System.Drawing.Point(43, 175);
             this.lbCountry.Name = "lbCountry";
             this.lbCountry.Size = new System.Drawing.Size(60, 20);
-            this.lbCountry.TabIndex = 5;
+            this.lbCountry.TabIndex = 4;
             this.lbCountry.Text = "Country";
             // 
-            // txtMemberID
+            // lbPassword
             // 
-            this.txtMemberID.Location = new System.Drawing.Point(194, 32);
-            this.txtMemberID.Name = "txtMemberID";
-            this.txtMemberID.Size = new System.Drawing.Size(178, 27);
-            this.txtMemberID.TabIndex = 6;
-            // 
-            // txtCity
-            // 
-            this.txtCity.Location = new System.Drawing.Point(194, 185);
-            this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(178, 27);
-            this.txtCity.TabIndex = 7;
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(194, 147);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(178, 27);
-            this.txtPassword.TabIndex = 8;
-            // 
-            // txtCountry
-            // 
-            this.txtCountry.Location = new System.Drawing.Point(194, 223);
-            this.txtCountry.Name = "txtCountry";
-            this.txtCountry.Size = new System.Drawing.Size(178, 27);
-            this.txtCountry.TabIndex = 9;
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Location = new System.Drawing.Point(194, 107);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(178, 27);
-            this.txtEmail.TabIndex = 10;
-            // 
-            // txtCompanyName
-            // 
-            this.txtCompanyName.Location = new System.Drawing.Point(194, 69);
-            this.txtCompanyName.Name = "txtCompanyName";
-            this.txtCompanyName.Size = new System.Drawing.Size(178, 27);
-            this.txtCompanyName.TabIndex = 11;
-            // 
-            // btnSave
-            // 
-            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnSave.Location = new System.Drawing.Point(57, 278);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(134, 45);
-            this.btnSave.TabIndex = 12;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(262, 278);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(134, 45);
-            this.btnCancel.TabIndex = 13;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(43, 210);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(70, 20);
+            this.lbPassword.TabIndex = 5;
+            this.lbPassword.Text = "Password";
             // 
             // frmMemberDetail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 368);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.txtCompanyName);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtCountry);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtCity);
-            this.Controls.Add(this.txtMemberID);
+            this.ClientSize = new System.Drawing.Size(436, 376);
+            this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.lbCountry);
             this.Controls.Add(this.lbCity);
-            this.Controls.Add(this.lbPassword);
-            this.Controls.Add(this.lbEmail);
             this.Controls.Add(this.lbCompanyName);
-            this.Controls.Add(this.lbMemberID);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbMemberId);
             this.Name = "frmMemberDetail";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Member Detail";
             this.Load += new System.EventHandler(this.frmMemberDetail_Load);
             this.ResumeLayout(false);
@@ -193,19 +111,11 @@
 
         #endregion
 
-        private Label lbMemberID;
-        private Label lbCompanyName;
+        private Label lbMemberId;
         private Label lbEmail;
-        private Label lbPassword;
+        private Label lbCompanyName;
         private Label lbCity;
         private Label lbCountry;
-        private TextBox txtMemberID;
-        private TextBox txtCity;
-        private TextBox txtPassword;
-        private TextBox txtCountry;
-        private TextBox txtEmail;
-        private TextBox txtCompanyName;
-        private Button btnSave;
-        private Button btnCancel;
+        private Label lbPassword;
     }
 }
